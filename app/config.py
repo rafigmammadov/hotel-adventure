de70a6c347ce8a56458a090e7499f54c8b4f9d0c
rafigmammadov <rafigmammadov@yahoo.com>
2023-11-05 01:41:18 +0400

    Some parameters has been changed

--- app/config.py
+++ app/config.py
@@ -1,10 +1,18 @@
-from dotenv import load_dotenv
-import os
+from pydantic_settings import BaseSettings, root_validator
 
-load_dotenv()
+class Settings(BaseSettings):
+    DB_HOST: int
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASS: str
 
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
+    @root_validator
+    def get_database_url(cls, v):
+        v["DATABASE_URL"] = f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
+        return v
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
