6d7535224c844ff80f9a182337f5f41a9e647ef8
rafigmammadov <rafigmammadov@yahoo.com>
2023-11-05 01:54:05 +0400

    The file config.py has been changed

--- app/config.py
+++ app/config.py
@@ -7,10 +7,9 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
 
-    @root_validator
-    def get_database_url(cls, v):
-        v["DATABASE_URL"] = f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
-        return v
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     class Config:
         env_file = ".env"
