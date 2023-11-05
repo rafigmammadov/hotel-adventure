b2f4ceab4c961414b28c66f363c891bed0312791
rafigmammadov <rafigmammadov@yahoo.com>
2023-11-05 17:41:17 +0400

    Some configurations have been changed

--- app/config.py
+++ app/config.py
@@ -1,17 +1,10 @@
-from pydantic_settings import BaseSettings, root_validator
+from dotenv import load_dotenv
+import os
 
-class Settings(BaseSettings):
-    DB_HOST: int
-    DB_PORT: int
-    DB_NAME: str
-    DB_USER: str
-    DB_PASS: str
+load_dotenv()
 
-    @property
-    def DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from app.config import settings
+from app.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-engine = create_async_engine(settings.DATABASE_URL)
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
